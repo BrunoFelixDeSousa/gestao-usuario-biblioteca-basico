d9297bd3db27797057685fc5024b3ff952bede61
brunofelixdesousa <brunofelixdesousa@hotmail.com>
2022-08-23 23:10:24 -0300

    add bootstrap

---
+++ src/main/java/com/bfs/gestaousuariosbiblioteca/GestaoUsuariosBibliotecaApplication.java
@@ -0,0 +1,13 @@
+package com.bfs.gestaousuariosbiblioteca;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestaoUsuariosBibliotecaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GestaoUsuariosBibliotecaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bfs/gestaousuariosbiblioteca/GestaoUsuariosBibliotecaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bfs.gestaousuariosbiblioteca;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestaoUsuariosBibliotecaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
