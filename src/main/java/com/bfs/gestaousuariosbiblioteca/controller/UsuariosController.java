4fb1e9f0699bfbf1a681309ee5521f8390db6a75
brunofelixdesousa <brunofelixdesousa@hotmail.com>
2022-08-25 00:59:23 -0300

    update ListaUsuarios.html

--- src/main/java/com/bfs/gestaousuariosbiblioteca/controller/UsuariosController.java
+++ src/main/java/com/bfs/gestaousuariosbiblioteca/controller/UsuariosController.java
@@ -5,20 +5,31 @@ import com.bfs.gestaousuariosbiblioteca.repository.Usuarios;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
+@RequestMapping("/usuarios")
 public class UsuariosController {
 
     @Autowired
     private Usuarios usuarios;
 
-    @GetMapping("/usuarios")
+    @GetMapping
     public ModelAndView listar() {
         ModelAndView modelAndView = new ModelAndView("ListaUsuarios");
         modelAndView.addObject("usuarios", usuarios.findAll());
 
+        modelAndView.addObject(new Usuario());
+
         return modelAndView;
+    }
+
+    @PostMapping
+    public String salvar(Usuario usuario) {
+        this.usuarios.save(usuario);
+        return "redirect:/usuarios";
     }
 
 }
